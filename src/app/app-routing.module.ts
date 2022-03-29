8071e4689e1e293188336d9f6e4550d89bd71f22
Kamil Pituła <pitula.k@gmail.com>
2022-03-29 18:45:48 +0200

    Create sign-in page draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './core/auth/sign-in/sign-in.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'topics', pathMatch: 'full'},
-  {path: 'topics', loadChildren: () => import('./topics/topics.module').then(m => m.TopicsModule)}
+  { path: '', redirectTo: 'topics', pathMatch: 'full' },
+  { path: 'topics', loadChildren: () => import('./topics/topics.module').then(m => m.TopicsModule) },
+  { path: 'signIn', component: SignInComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './core/auth/auth.module';
 import { LayoutModule } from './layout/layout.module';
 import { TopicsModule } from './topics/topics.module';
 
@@ -14,7 +15,8 @@ import { TopicsModule } from './topics/topics.module';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    TopicsModule
+    TopicsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
