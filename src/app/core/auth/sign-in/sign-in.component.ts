8e682257d9797e9a9eee481760795426706a2702
Kamil Pituła <pitula.k@gmail.com>
2022-03-29 19:30:23 +0200

    Crate login reactive form with simple validators.

--- src/app/core/auth/sign-in/sign-in.component.ts
+++ src/app/core/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
+  
+  loginData = new FormGroup({
+    login: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSignIn() {
+    console.warn(this.loginData.value);
   }
-
 }
