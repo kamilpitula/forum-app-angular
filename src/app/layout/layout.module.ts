772a9abfa60a7561da18d5f4298a9a1119f7a619
Kamil Pituła <pitula.k@gmail.com>
2022-03-30 18:49:34 +0200

    move buttons to controls

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavButtonComponent } from './buttons/nav-button/nav-button.component';
+import { NavButtonComponent } from './controls/buttons/nav-button/nav-button.component';
 import { RouterModule } from '@angular/router';
 import { BackNavBarComponent } from './controls/back-nav-bar/back-nav-bar.component';
 import { InformationPanelComponent } from './controls/information-panel/information-panel.component';
